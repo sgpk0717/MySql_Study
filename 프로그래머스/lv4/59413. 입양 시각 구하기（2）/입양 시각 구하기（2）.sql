2cb063682265afc272d41429eee85fa3f2022c16
Seong-Guk Park <38274677+sgpk0717@users.noreply.github.com>
2023-08-20 16:12:04 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,10 @@
+SET @VAR1 := -1;
+
+SELECT  @VAR1 := @VAR1 + 1 AS HOUR,
+        (SELECT COUNT(B.ANIMAL_ID)
+         FROM   ANIMAL_OUTS B
+         WHERE  1=1
+         AND    DATE_FORMAT(B.DATETIME,'%H') = @VAR1) AS COUNT
+FROM    ANIMAL_OUTS 
+WHERE   @VAR1 < 23
+;
